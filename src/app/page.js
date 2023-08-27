98e5e205aa42e1923f3a11fd65b42ab11c91f742
WebRizen <webrizen@gmail.com>
2023-08-27 18:37:55 +0530

    that's great

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import LatestPost from '@/components/posts/LatestPost';
 import SixPosts from '@/components/posts/SixPosts';
 import TrendingPosts from '@/components/posts/TrendingPosts';
 import styles from '@/styles/home.module.css';
+import Link from 'next/link';
 
 // Fetch the latest post data from the API
 async function fetchLatestPostData() {
@@ -23,9 +24,21 @@ async function fetchTrendingPostData() {
   return data.blogPosts.slice(1);
 }
 
+//fetch categories data from URL:
+async function fetchCategories() {
+  const response = await fetch("https://healthcare-biodiversity.vercel.app/api/categories", { cache: 'force-cache' }, { next: { revalidate: 3600 } });
+  if (!response.ok) {
+    throw new Error("Failed to fetch categories data");
+  }
+  const data = await response.json();
+  return data.categories;
+}
+
+
 export default async function Home() {
   const latestPostData = await fetchLatestPostData();
   const trendingPostData = await fetchTrendingPostData();
+  const categories = await fetchCategories();
 
   return (
     <section className={styles.Home}>
@@ -38,7 +51,15 @@ export default async function Home() {
         <div className={styles.cattfatty}>
           <h2>Categories</h2>
           <hr />
-          Categories Contents...
+          <ul>
+          {categories.map((category) => (
+              <li key={category.id} className='py-2 hover:opacity-60'>
+                <Link href={`/categories/${category.permalink}`}>
+                  {category.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </section>
