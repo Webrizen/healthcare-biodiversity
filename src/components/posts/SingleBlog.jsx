f0daee67aef4d1eeb99947b35f6a49a78842b783
WebRizen <webrizen@gmail.com>
2023-08-30 09:17:47 +0530

    keywords added below and responsiveness

--- src/components/posts/SingleBlog.jsx
+++ src/components/posts/SingleBlog.jsx
@@ -10,7 +10,13 @@ import {
 } from "firebase/firestore";
 import firebase_app from "@/firebase/config";
 import Image from "next/image";
-import { Typography, Avatar, IconButton, Button, Spinner } from "@material-tailwind/react";
+import {
+  Typography,
+  Avatar,
+  IconButton,
+  Button,
+  Spinner,
+} from "@material-tailwind/react";
 import { BsDot, BsFacebook, BsLinkedin, BsTwitter } from "react-icons/bs";
 import { FiMail, FiLink } from "react-icons/fi";
 import Swal from "sweetalert2";
@@ -34,7 +40,7 @@ async function fetchAuthorImage(authorName) {
   const data = await response.json();
 
   const author = data.find((author) => author.name === authorName);
-  return author?.profileImageUrl || "/placeholder.svg"; 
+  return author?.profileImageUrl || "/placeholder.svg";
 }
 
 async function fetchAuthorId(authorName) {
@@ -78,7 +84,6 @@ export default function singleBlog({ id }) {
   // Call the function when the component mounts
   incrementViewCount();
 
-  
   function formatNumber(number) {
     const billion = 1000000000;
     const million = 1000000;
@@ -154,10 +159,10 @@ export default function singleBlog({ id }) {
   function timeAgo(timestamp) {
     const currentDate = new Date();
     const providedDate = new Date(timestamp.seconds * 1000);
-  
+
     const timeDifference = currentDate - providedDate;
     const seconds = Math.floor(timeDifference / 1000);
-  
+
     if (seconds < 60) {
       return `${seconds} seconds ago`;
     } else if (seconds < 3600) {
@@ -181,7 +186,9 @@ export default function singleBlog({ id }) {
   return (
     <>
       <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16 relative">
-      <h1 className="text-5xl font-medium mb-4">{blogData?.title || <Spinner/>}</h1>
+        <h1 className="text-5xl font-medium mb-4">
+          {blogData?.title || <Spinner />}
+        </h1>
         <div className="bg-cover text-center overflow-hidden rounded-xl">
           <figure>
             <Image
@@ -192,24 +199,35 @@ export default function singleBlog({ id }) {
               className="w-full rounded-xl"
             />
             <figcaption className="mt-2 text-center font-normal">
-            {blogData?.title || 'Image caption'}
+              {blogData?.title || "Image caption"}
             </figcaption>
           </figure>
         </div>
         <div className="max-w-screen-xl mx-auto my-5">
-          <div className="top-content my-4 flex flex-row justify-between items-center">
-            <div className="flex items-center gap-4">
-            <Avatar src={AuthorImage || '/placeholder.svg'} alt="Author Profile PIC" />
-              <div>
+          <div className="top-content my-4 flex flex-col gap-4 md:flex-row md:justify-between md:items-center">
+            <div className="flex items-center gap-4 md:gap-4">
+              <Avatar
+                src={AuthorImage || "/placeholder.svg"}
+                alt="Author Profile PIC"
+              />
+              <div className="flex flex-col">
                 <Link href={`/authors/${authorId}`}>
-                <Typography variant="h6">{blogData?.author || <Spinner/>}</Typography>
+                  <Typography variant="h6">
+                    {blogData?.author || <Spinner />}
+                  </Typography>
                 </Link>
                 <Typography
                   variant="small"
                   color="gray"
-                  className="font-normal flex flex-row gap-1 items-center justify-center"
+                  className="font-normal flex flex-row gap-1 items-center justify-center md:text-sm text-xs"
                 >
-                  {blogData?.timestamp ? `Posted ${timeAgo(blogData.timestamp)}` : 'Posted - ago'} <BsDot /> {blogData?.categories || <Spinner/>}
+                  <sapn className="whitespace-nowrap">
+                    {blogData?.timestamp
+                      ? `Posted ${timeAgo(blogData.timestamp)}`
+                      : "Posted - ago"}
+                  </sapn>
+                  <BsDot />
+                  {blogData?.categories || <Spinner />}
                 </Typography>
               </div>
             </div>
@@ -231,17 +249,38 @@ export default function singleBlog({ id }) {
               </IconButton>
             </div>
           </div>
+
           <div className="mt-3 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
-            <div 
-            className="content"
-            dangerouslySetInnerHTML={{ __html: blogData?.content || <Spinner/> }}
-             />
+            <div
+              className="content"
+              dangerouslySetInnerHTML={{
+                __html: blogData?.content || <Spinner />,
+              }}
+            />
+          </div>
+
+          <div className="mt-5 flex flex-row gap-2 justify-left items-center">
+            {blogData && blogData.keywords ? (
+              blogData.keywords.split(",").map((keyword, index) => (
+                <span
+                  key={index}
+                  className="bg-gray-200 text-gray-800 px-2 py-1 rounded-full text-xs"
+                >
+                  {keyword.trim()}
+                </span>
+              ))
+            ) : (
+              <Spinner />
+            )}
           </div>
         </div>
       </div>
       <div className="flex gap-4 w-min p-2 rounded-3xl mx-auto sticky bottom-2 left-0 right-0">
-      <Button variant="text" className="flex flex-row gap-1 items-center rounded-full">
-      <svg
+        <Button
+          variant="text"
+          className="flex flex-row gap-1 items-center rounded-full"
+        >
+          <svg
             className="w-4 h-4 mr-1"
             stroke="currentColor"
             strokeWidth="2"
@@ -254,9 +293,13 @@ export default function singleBlog({ id }) {
             <circle cx="12" cy="12" r="3"></circle>
           </svg>
           <span>{formatNumber(viewCount)}</span>
-      </Button>
-      <Button variant="gradient" className="flex flex-row gap-1 items-center rounded-full" onClick={incrementLikeCount}>
-      <svg
+        </Button>
+        <Button
+          variant="gradient"
+          className="flex flex-row gap-1 items-center rounded-full"
+          onClick={incrementLikeCount}
+        >
+          <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
@@ -271,7 +314,7 @@ export default function singleBlog({ id }) {
             />
           </svg>
           <span>{formatNumber(likeCount)}</span>
-      </Button>
+        </Button>
       </div>
     </>
   );
